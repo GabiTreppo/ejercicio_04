a508d5704341c6e9f99b169acb8479a243d4a75d
Gabriel <gabrieltreppo3@gmail.com>
2022-06-28 18:37:33 -0300

    agregado index y carpetas: css, img, js y pages

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let nombre= prompt("ingrese su nombre")
+console.log(nombre)
+let genero = prompt("ingrese su género: 1 hombre, 2 mujer, 3 no binario, 4 no especificar.")
+if(genero ==1){
+    console.log("bienvenido")
+}
+if(genero == 2){ 
+    console.log("bienvenida")
+}
+if(genero == 3) {
+    console.log("bienvenide")
+}
+else{
+    console.log("bienvenid@")
+}
+
